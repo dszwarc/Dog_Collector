94dda139f988f2919d6852f9b824fc005df663a1
dszwarc <dszwarc93@gmail.com>
2023-04-07 16:14:07 -0400

    add about page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,20 +1,11 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('dogs/', views.dog_index, name='dog_index', {dogs: dogs}),   
+    path('about/', views.about, name="about"),
+    path('dogs/', views.dog_index, name='dog_index'),   
 ]
 
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Appa', 'corgi', 'The goodest little boi', 5),
-  Dog('Kropka', 'beagle', 'My first dog <3', 18),
-  Dog('Chip', 'unknown mix', "Appa's least favorite cousin.", 4)
-]
--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,21 @@ def home(request):
     return render(request, 'home.html')
 
 def dog_index(request):
-    return render(request, 'dogs/index.html')
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def about(request):
+    return render(request, 'about.html')
+
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Appa', 'Corgi', 'The goodest little boi', 5),
+  Dog('Kropka', 'Beagle', 'My first dog <3', 18),
+  Dog('Chip', 'Unknown mix', "Appa's least favorite cousin.", 4),
+  Dog('Momo', 'Dachshund', "Appa's brother/sister that he doesn't know about.", 0)
+]
