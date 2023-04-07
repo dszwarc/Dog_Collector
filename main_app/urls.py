dfe9315f3384043e1b0723132de7804e732cf85f
dszwarc <dszwarc93@gmail.com>
2023-04-07 15:34:08 -0400

    added dog index html and created three dog 'models'

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('dogs/', views.dog_index, name='dog_index'),   
+    path('dogs/', views.dog_index, name='dog_index', {dogs: dogs}),   
+]
+
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Appa', 'corgi', 'The goodest little boi', 5),
+  Dog('Kropka', 'beagle', 'My first dog <3', 18),
+  Dog('Chip', 'unknown mix', "Appa's least favorite cousin.", 4)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def dog_index(request):
+    return render(request, 'dogs/index.html')
