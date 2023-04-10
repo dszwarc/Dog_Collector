6cb252ffe94b65739a8539871d19dc492dbc34af
dszwarc <dszwarc93@gmail.com>
2023-04-10 11:59:43 -0400

    added model for Dog

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Dog
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def dog_index(request):
+    dogs = Dog.models.get()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
 def about(request):
     return render(request, 'about.html')
 
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Appa', 'Corgi', 'The goodest little boi', 5),
-  Dog('Kropka', 'Beagle', 'My first dog <3', 18),
-  Dog('Chip', 'Unknown mix', "Appa's least favorite cousin.", 4),
-  Dog('Momo', 'Dachshund', "Appa's brother/sister that he doesn't know about.", 0)
-]
+# dogs = [
+#   Dog('Appa', 'Corgi', 'The goodest little boi', 5),
+#   Dog('Kropka', 'Beagle', 'My first dog <3', 18),
+#   Dog('Chip', 'Unknown mix', "Appa's least favorite cousin.", 4),
+#   Dog('Momo', 'Dachshund', "Appa's brother/sister that he doesn't know about.", 0)
+# ]
