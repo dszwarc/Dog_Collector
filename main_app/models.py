784b5996198501c9af35bc9d6db77751c4e1d5b1
dszwarc <dszwarc93@gmail.com>
2023-04-12 11:05:19 -0400

    add toy forms and toy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.urls import reverse
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
